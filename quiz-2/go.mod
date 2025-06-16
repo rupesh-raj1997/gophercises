6646f43a541fed9e0a24cd658fa6e54b0dbbc7f3
zigmint <rupeshr053@gmail.com>
2025-06-16 11:34:52 +0530

    chore: complete quiz-2

---
+++ quiz-2/go.mod
@@ -0,0 +1,5 @@
+module quiz-2
+
+go 1.23.6
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ quiz-2/urlshort/handler.go
@@ -0,0 +1,62 @@
+package urlshort
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+type pathToUrl struct {
+	path string `yaml:"path"`
+	url  string `yaml:"url"`
+}
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	//	TODO: Implement this...
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if dest, ok := pathsToUrls[r.URL.Path]; ok {
+			http.Redirect(w, r, dest, http.StatusSeeOther)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	})
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var data []pathToUrl
+	var pathsToUrls map[string]string
+	err := yaml.Unmarshal(yml, &data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, d := range data {
+		if d.path != "" && d.url != "" {
+			pathsToUrls[d.path] = d.url
+		}
+	}
+	return MapHandler(pathsToUrls, fallback), nil
+}
